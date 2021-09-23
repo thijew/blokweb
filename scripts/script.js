64ce43424860f294d2de08cc69c48e648b56e877
thijew <90254525+thijew@users.noreply.github.com>
2021-09-23 21:51:19 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+var deOpenButton = document.querySelector("header > button");
+var deNav = document.querySelector("nav");
+var deSluitButton = document.querySelector("header nav button");
+
+deOpenButton.addEventListener("click", toggleMenu);
+deSluitButton.addEventListener("click", toggleMenu);
+
+function toggleMenu () {
+    deNav.classList.toggle("toonMenu")
+}
